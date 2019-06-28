b6316cf12add9461af1f001ce9393f5553d2c8dc
pietromoretto <pietromoretto7@gmail.com>
2019-06-28 17:28:12 +0200

    Changed Directories

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -53,12 +53,10 @@ def csv_crop(img, row):
     img = img[ymin:ymax, xmin:xmax]
     return (img, row["name"])
 '''    
-plaque_images = "./Data/Patches/"
+plaque_images = "./Data/Patches/Plaque/"
 patch_images = "./Data/Patches/NoPlaque/"
-aug_images =["./Data/Aug0/", "./Data/Aug1/"]
-aug_num = 0
+aug_images ="./Data/Patches/Plaque/Aug/"
 folder = (os.path.join(plaque_images, file_path) for file_path in os.listdir(plaque_images))
-aug_num = 1
 for counter, file_path in enumerate(folder):
     print(file_path)
     if('.png' in file_path):
@@ -90,18 +88,18 @@ for counter, file_path in enumerate(folder):
                     np.save(aug_images[aug_num-1] + file_path.split("/")[-1][:-4]+"_"+cls+ct+"_dflip.npy", img_dflip)
         '''
         img = rsz(img, 64,64)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+".npy", img)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+".npy", img)
         img_rot90 = rot(img, 90)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_rot90.npy", img_rot90)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_rot90.npy", img_rot90)
         img_rot270 = rot(img, 270)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_rot270.npy", img_rot270)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_rot270.npy", img_rot270)
         img_hflip = hflip(img)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_hflip.npy", img_hflip)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_hflip.npy", img_hflip)
         img_vflip = vflip(img)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_vflip.npy", img_vflip)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_vflip.npy", img_vflip)
         img_dflip = dflip(img)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_dflip.npy", img_dflip)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_dflip.npy", img_dflip)
         img_gauss = gauss(img, 10)
-        np.save(aug_images[aug_num] + file_path.split("/")[-1][:-4]+"_gauss10.npy", img_gauss)
+        np.save(aug_images + file_path.split("/")[-1][:-4]+"_gauss10.npy", img_gauss)
         #img_gauss = gauss(img_, 20)
         #np.save(aug_images + file_path.split("/")[-1][:-4]+"_gauss20.npy", img_gauss)
